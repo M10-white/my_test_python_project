e4676681a48443fa5a21b59e24f34fd0921f0ad3
Brahim CHAOUKI <bchaouki00@gmail.com>
2025-05-12 17:12:48 +0200

    fix: adapt to flake8

--- TP2/test_main.py
+++ TP2/test_main.py
@@ -1,19 +1,20 @@
-
-
-
 from main import hello
 import pytest
 import time
 
+
 def test_hello():
     assert hello() == "Hello, John Doe!"
+
 
 def test_hello_custom_name():
     assert hello("EPSI") == "Hello, EPSI Doe!"
 
+
 def test_hello_type_error():
     with pytest.raises(TypeError):
         hello(123)
+
 
 def test_hello_performance():
     start = time.time()
@@ -21,6 +22,7 @@ def test_hello_performance():
         hello("EPSI")
     duration = time.time() - start
     assert duration < 1
+
 
 def test_hello_full_name():
     assert hello("Jane", "Smith") == "Hello, Jane Smith!"
